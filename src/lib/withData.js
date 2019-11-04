2ed3fb7edd3ae9cc4efd89fce9fd1fc233cfd63f
Nosakhare Emmanuel <nonreactiv@gmail.com>
2019-11-04 14:11:02 +0100

    Fix withData HOC environment
    create new environment once url changes

--- src/lib/withData.js
+++ src/lib/withData.js
@@ -1,96 +1,101 @@
 import { ViewerProvider } from './withViewer'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import createEnvironment from 'relay-environment'
 import { fetchQuery } from 'react-relay'
 import RelayProvider from './RelayProvider'
 import { NotFound } from 'views/user/NotFound'
+import { withRouter } from 'next/router'
 
 export function withData(ComposedComponent, options = {}) {
-  return class WithData extends React.Component {
-    static displayName = `WithData(${ComposedComponent.displayName})`
+  function WithData(props) {
+    const { variables, viewer, queryRecords } = props
 
-    static async getInitialProps(ctx) {
-      let composedInitialProps = {}
-      if (ComposedComponent.getInitialProps) {
-        composedInitialProps = await ComposedComponent.getInitialProps(ctx)
-      }
+    const [environment, setEnvironment] = useState(
+      createEnvironment({ records: queryRecords })
+    )
 
-      if (!options.query) return composedInitialProps
+    useEffect(() => {
+      setEnvironment(createEnvironment({ records: queryRecords }))
+    }, [props.router.asPath])
 
-      let queryProps = {}
-      let queryRecords = {}
-      let variables = {}
-      let config = {}
-
-      if (!process.browser) {
-        config = {
-          cache: 'no-cache',
-          headers: {
-            cookie: ctx.req.headers.cookie
-          }
-        }
-      }
-
-      const environment = createEnvironment(config)
-
-      if (options.query) {
-        const url = { query: ctx.query, pathname: ctx.pathname }
-        variables = { ...url.query, ...options.variables }
-        queryProps = await fetchQuery(environment, options.query, variables)
-
-        queryRecords = environment
-          .getStore()
-          .getSource()
-          .toJSON()
-      }
-
-      return {
-        ...composedInitialProps,
-        ...queryProps,
-        variables,
-        queryRecords
-      }
-    }
-
-    constructor(props) {
-      super(props)
-      this.environment = createEnvironment({
-        records: props.queryRecords
-      })
-    }
-
-    pageFufillsExpectation(expectation) {
-      const { viewer } = this.props
+    function pageFufillsExpectation(expectation) {
       if (expectation === 'viewer' && !(viewer && viewer.username)) return -1
-      if (expectation && !this.props[expectation]) return false
+      if (expectation && !props[expectation]) return false
       return true
     }
 
-    render() {
-      const { variables, viewer } = this.props
-      let expectViewer
+    let expectViewer
 
-      if (Array.isArray(options.expect)) {
-        for (let expectation of options.expect) {
-          const result = this.pageFufillsExpectation(expectation)
-          if (result === -1) expectViewer = true
-          else if (!result) return <NotFound />
-        }
-      } else {
-        const result = this.pageFufillsExpectation(options.expect)
+    if (Array.isArray(options.expect)) {
+      for (let expectation of options.expect) {
+        const result = pageFufillsExpectation(expectation)
         if (result === -1) expectViewer = true
         else if (!result) return <NotFound />
       }
+    } else {
+      const result = pageFufillsExpectation(options.expect)
+      if (result === -1) expectViewer = true
+      else if (!result) return <NotFound />
+    }
 
-      return (
-        <RelayProvider environment={this.environment} variables={variables}>
-          <ViewerProvider expectViewer={expectViewer} viewer={this.props}>
-            <ComposedComponent {...this.props} />
-          </ViewerProvider>
-        </RelayProvider>
-      )
+    return (
+      <RelayProvider environment={environment} variables={variables}>
+        <ViewerProvider expectViewer={expectViewer} viewer={props}>
+          <ComposedComponent {...props} />
+        </ViewerProvider>
+      </RelayProvider>
+    )
+  }
+
+  const { displayName, name } = ComposedComponent
+
+  WithData.displayName = `withData(${displayName || name})`
+
+  WithData.getInitialProps = async ctx => {
+    let composedInitialProps = {}
+
+    if (ComposedComponent.getInitialProps) {
+      composedInitialProps = await ComposedComponent.getInitialProps(ctx)
+    }
+
+    if (!options.query) return composedInitialProps
+
+    let queryProps = {}
+    let queryRecords = {}
+    let variables = {}
+    let config = {}
+
+    if (!process.browser) {
+      config = {
+        cache: 'no-cache',
+        headers: {
+          ...ctx.req.headers
+        }
+      }
+    }
+
+    const environment = createEnvironment(config)
+
+    if (options.query) {
+      const url = { query: ctx.query, pathname: ctx.pathname }
+      variables = { ...url.query, ...options.variables }
+      queryProps = await fetchQuery(environment, options.query, variables)
+
+      queryRecords = environment
+        .getStore()
+        .getSource()
+        .toJSON()
+    }
+
+    return {
+      ...composedInitialProps,
+      ...queryProps,
+      variables,
+      queryRecords
     }
   }
+
+  return withRouter(WithData)
 }
 
 export default withData
