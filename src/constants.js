ad1150fb10df3d2171f870b90bab29c19db259cf
Nosakhare Emmanuel <nonreactiv@gmail.com>
2019-12-25 14:08:22 +0100

    Add now secrets

--- src/constants.js
+++ src/constants.js
@@ -2,19 +2,6 @@ const dev = process.env.NODE_ENV === 'development'
 const isServer = typeof window === 'undefined'
 const LOCALHOST = !isServer ? window.location.hostname : 'localhost'
 
-export const Constants = {
-  DEV: dev
-}
-
-export const WEBSITE_URL = dev
-  ? `http://${LOCALHOST}:3000`
-  : 'https://thecommunity.ng'
-
-let DATA_URL =
-  process.env.BACKEND_URL ||
-  (Constants.DEV ? `http://${LOCALHOST}:5000/` : 'https://secure.thecommunity.ng/')
-
-if (process.browser && location.hostname.split('.')[0] === 'staging')
-  DATA_URL = 'https://staging-api.thecommunity.ng/'
-
-export { DATA_URL }
+export const Constants = { DEV: dev }
+export const WEBSITE_URL = process.env.SITE_HOST
+export const DATA_URL = `${process.env.API_HOST}/`
