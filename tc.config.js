ed6df05d228003b0940934fffcd97797b30a20f8
Emmanuel Nosakhare <6781915+osarogie@users.noreply.github.com>
2024-08-01 00:51:17 +0100

    chore: cleanup

--- tc.config.js
+++ tc.config.js
@@ -1,10 +1,9 @@
 const CLIENT_URL =
   process.env.NEXT_PUBLIC_CLIENT_URL || 'https://thecommunity.ng'
 export const BACKEND_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || 'https://hq.thecommunity.ng'
+  process.env.NEXT_PUBLIC_BACKEND_URL || 'https://api.thecommunity.ng'
 
-export const GRAPHQL_ENDPOINT = `${BACKEND_URL}/api/v2/graphql`;
-export const apiBaseUrl = `${BACKEND_URL}/`;
+export const GRAPHQL_ENDPOINT = `${BACKEND_URL}/api/v2/graphql`
 export const baseUrl = CLIENT_URL
 export const secureBaseUrl = BACKEND_URL
 export const backendUrl = BACKEND_URL
