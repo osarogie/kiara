5743287ecdbc1856e8c6d9b6952317f49572d4b9
Emmanuel Nosakhare <6781915+osarogie@users.noreply.github.com>
2024-08-01 10:09:04 +0100

    chore: cleanup

---
+++ artifacts/relay/SidebarPopularUsersQuery.graphql.ts
@@ -0,0 +1,199 @@
+/**
+ * @generated SignedSource<<a0b942c3f4342cc79806b23010b34cae>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+export type SidebarPopularUsersQuery$variables = {};
+export type SidebarPopularUsersQuery$data = {
+  readonly popularUsers: {
+    readonly edges: ReadonlyArray<{
+      readonly node: {
+        readonly _id: string;
+        readonly bio: string | null;
+        readonly name: string | null;
+        readonly profilePicture: string | null;
+        readonly profilePictureName: string | null;
+        readonly username: string | null;
+      } | null;
+    } | null> | null;
+  } | null;
+};
+export type SidebarPopularUsersQuery = {
+  response: SidebarPopularUsersQuery$data;
+  variables: SidebarPopularUsersQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "first",
+    "value": 5
+  }
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "_id",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "username",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "bio",
+  "storageKey": null
+},
+v5 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "profilePicture",
+  "storageKey": null
+},
+v6 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "profilePictureName",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "SidebarPopularUsersQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "UserConnection",
+        "kind": "LinkedField",
+        "name": "popularUsers",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "UserEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "User",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v1/*: any*/),
+                  (v2/*: any*/),
+                  (v3/*: any*/),
+                  (v4/*: any*/),
+                  (v5/*: any*/),
+                  (v6/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": "popularUsers(first:5)"
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "SidebarPopularUsersQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "UserConnection",
+        "kind": "LinkedField",
+        "name": "popularUsers",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "UserEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "User",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v1/*: any*/),
+                  (v2/*: any*/),
+                  (v3/*: any*/),
+                  (v4/*: any*/),
+                  (v5/*: any*/),
+                  (v6/*: any*/),
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "id",
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": "popularUsers(first:5)"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "0023c8efedb0e43b65d8e8ad39fd593a",
+    "id": null,
+    "metadata": {},
+    "name": "SidebarPopularUsersQuery",
+    "operationKind": "query",
+    "text": "query SidebarPopularUsersQuery {\n  popularUsers(first: 5) {\n    edges {\n      node {\n        _id\n        name\n        username\n        bio\n        profilePicture\n        profilePictureName\n        id\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "5f112edbed4f6c8fe1055b7b91811058";
+
+export default node;
---
+++ src/views/feed/sidebar/Sidebar.tsx
@@ -0,0 +1,164 @@
+import { SecureLink } from '../../../components/SecureLink'
+import Avatar from 'components/Avatar'
+import { View, Text } from 'react-native-web'
+import { newStoryLink, newPoll, newGroup } from '../../../helpers/links'
+import Anchor from 'antd/lib/anchor'
+import { graphql, useLazyLoadQuery } from 'react-relay'
+import { SidebarPopularUsersQuery } from '@artifacts/relay/SidebarPopularUsersQuery.graphql'
+import Link from 'next/link'
+
+const popularUsersQuery = graphql`
+  query SidebarPopularUsersQuery {
+    popularUsers(first: 5) {
+      edges {
+        node {
+          _id
+          name
+          username
+          bio
+          profilePicture
+          profilePictureName
+        }
+      }
+    }
+  }
+`
+
+function PopularUsers() {
+  const data = useLazyLoadQuery<SidebarPopularUsersQuery>(popularUsersQuery, {})
+
+  return (
+    <div
+      className="sidebar r-side extra-padding s__main__bg bd s__content__main"
+      style={{ margin: '0 0 15px 15px' }}
+    >
+      <p>Who to follow</p>
+      {data?.popularUsers?.edges?.map((user) => (
+        <View
+          key={user!.node!._id}
+          style={{
+            flexDirection: 'row',
+            flex: 1,
+            alignItems: 'center',
+            marginBottom: 10
+          }}
+        >
+          <Avatar
+            size={40}
+            rounded
+            source={user!.node}
+            title={user!.node!.name}
+            activeOpacity={0.7}
+          />
+          <Link href={`/${user!.node!.username}`}>
+            <Text
+              numberOfLines={2}
+              style={{
+                flex: 1,
+                marginLeft: 10,
+                fontSize: 13
+              }}
+            >
+              {user!.node!.name}
+            </Text>
+          </Link>
+        </View>
+      ))}
+    </div>
+  )
+}
+
+export function Sidebar() {
+  return (
+    <Anchor style={{ backgroundColor: 'transparent', marginTop: 50 }}>
+      <div className="side">
+        <div className="table">
+          <PopularUsers />
+          <div
+            className="sidebar r-side extra-padding s__main__bg bd"
+            style={{ margin: '0 0 15px 15px' }}
+          >
+            <div className="s-top s__content__main">
+              <div>
+                <SecureLink
+                  message="Login to share your story"
+                  href={newStoryLink()}
+                  className="s-button full"
+                >
+                  Share Your Story
+                </SecureLink>
+              </div>
+              <div>
+                <SecureLink
+                  message="Login to create a voting poll"
+                  href={newPoll()}
+                  className="s-button full"
+                >
+                  New voting poll
+                </SecureLink>
+              </div>
+              <div>
+                <SecureLink
+                  message="Login to start your own blog"
+                  href={newGroup()}
+                  className="s-button full"
+                >
+                  Start your own blog
+                </SecureLink>
+              </div>
+            </div>
+            {/* <div className="d-embed">
+              <div className="inner">
+                <div className="table">Confirm your email address</div>
+                <br />
+                <a href="/a/confirmation" className="button table">
+                  Click to confirm
+                </a>
+              </div>
+            </div> */}
+            <br />
+            <br />
+            <div className="s__content__main80">
+              <a
+                style={{
+                  fontSize: 20,
+                  fontFamily: "'Karla', sans-serif"
+                }}
+                href="/"
+              >
+                <span className="s__content__main">The</span>
+                <span className="s__content__main">Community</span>
+              </a>
+              <br />
+              <small>Made with &hearts; in Lagos</small>
+              <br />
+              <small className="ia">
+                &copy; {new Date().getFullYear()} TheCommuntiy
+              </small>
+              <small className="ia">
+                <a className="uh" href="/privacy">
+                  Privacy
+                </a>
+              </small>
+              <small>
+                <a
+                  className="uh"
+                  href="https://play.google.com/store/apps/details?id=ng.thecommunity.kovu&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+                  target="blank"
+                >
+                  {/* <img
+                  alt="Get it on Google Play"
+                  className="playstore_badge"
+                  src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png"
+                /> */}
+                  Android
+                </a>
+              </small>
+            </div>
+            P
+          </div>
+        </div>
+      </div>
+    </Anchor>
+  )
+}
